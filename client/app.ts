4ac413d321ecaf01f9567db0120a65015ac96613
Xavier Folch <Xavier Folch>
2016-01-31 22:27:46 +0100

    step 4 done

--- client/app.ts
+++ client/app.ts
@@ -1,14 +1,15 @@
 import {Component, View, NgZone} from 'angular2/core';
 
 import {bootstrap} from 'angular2-meteor';
-
+import {PartiesForm} from 'client/parties-form/parties-form';
 import {Parties} from 'collections/parties';
 
 @Component({
     selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html'
+    templateUrl: 'client/app.html',
+    directives: [PartiesForm]
 })
 class Socially {
 	parties: Mongo.Cursor<Object>;
@@ -16,6 +17,10 @@ class Socially {
 	constructor (zone: NgZone) {
 		this.parties = Parties.find();
 	}
+
+	removeParty(party) {
+        Parties.remove(party._id);
+    }
 }
 
 bootstrap(Socially);
